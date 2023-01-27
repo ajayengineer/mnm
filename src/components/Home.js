c7bf93e6e35002a978d027badf06e497fd83f45c
ajayengineer <ajaywebsolution@gmail.com>
2023-01-26 19:54:56 -0800

    Tree Menu and Forms updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import GeographicalHierarchy from '../features/geographicalhierarchy/GeographicalHierarchy'
+import TreeMenu from './TreeMenu';
+import TreeSearch from './TreeSearch';
+import GoSubmitForm from './GoSubmitForm';
+import GoUpdateForm from './GoUpdateForm';
 
 function Home() {
     return (
-        <div>
-            <GeographicalHierarchy />
+        <div className='container my-5'>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <TreeSearch />
+                    <TreeMenu />
+                </div>
+                <div className='col-md-6'>
+                    <div className='col-md-12'>
+                        <GoSubmitForm />
+                    </div>
+                    <div className='col-md-12'>
+                        <GoUpdateForm />
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
